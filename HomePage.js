1c27c428657dfd666fefe846163e71d5be605538
NortonAntiviral <officialantiviral@gmail.com>
2019-01-04 22:59:21 -0500

    added some basic js

---
+++ HomePage.js
@@ -0,0 +1,21 @@
+var loginBtn = document.querySelector("#Login");
+var login = document.querySelector("loginScreen");
+
+// alert("CONNECTED");
+
+function initialize() {
+  setUpButtons();
+}
+
+function setUpButtons() {
+loginBtn.addEventListener("click",function() {
+
+alert("CONNECTED");
+
+// login.className += ".is-active";
+
+
+
+}
+);
+}
