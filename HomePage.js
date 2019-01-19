0bb3b458df984e9fcd36de004c2adb0b4bea61dd
NortonAntiviral <officialantiviral@gmail.com>
2019-01-18 22:19:39 -0500

    added modal with basic functionality and JS activation to all buttons

--- HomePage.js
+++ HomePage.js
@@ -7,39 +7,36 @@ var loginBtn = document.getElementById("login");
 var loginScreen = document.getElementById('popUp');
 
 //Close Button
-var closeBtn = document.getElementById('close');
+var closeBtn = document.getElementsByClassName('delete')[0];
 
 // LOGIN BUTTON
 loginBtn.onclick = function LogInFunction () {
   // console.log('Log In Connected');
 
-  if(loginScreen.className == 'is-active') {
-  
-    return;
-  } else {
       //activate the Modal
-    loginScreen.className = "is-active";
-  }
+
+    loginScreen.style.display = 'block';
   
 };
+
+// SIGN UP
+signUp.onclick = function signUpFunction () {
+  console.log('Sign Up Connected!');
+};
+
+// SEARCH BUTTON
+searchBtn.onclick = function Search (){
+  console.log('Seach Activated');
+}
 
 // CLOSE MODALS AND POPUPS
 
 // CLOSE LOGIN MODAL
 
-closeBtn.addEventListener('submit',function () {
-  loginScreen.classList.remove("is-active");
-});
+closeBtn.onclick = function () {
+  loginScreen.style.display = 'none';
+}
 
-// closeBtn.onclick = function () {
-//   if(loginScreen.className == 'is-active') {
-//   loginScreen.classList.remove("is-active");
-    
-//   } else {
-//     return;
-//   }
-// };
 
-signUp.onclick = function signUpFunction () {
-  console.log('Sign Up Connected!');
-};
+
+
